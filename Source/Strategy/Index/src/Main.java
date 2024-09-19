864ab28d1862eaf37fd562f25dc6a2233a9887b6
RezaRfahi <refahireza82@gmail.com>
2024-09-20 01:34:28 +0330

    Successful final main output

--- Source/Strategy/Index/src/Main.java
+++ Source/Strategy/Index/src/Main.java
@@ -2,7 +2,13 @@ import FeeCalculation.*;
 
 public class Main {
     public static void main(String[] args) {
-        BookLateFee book = new BookLateFee();
-        System.out.println(book.getMaxLateFee());
+        LibraryItem book = new LibraryItem("The Great Gatsby", new BookLateFee());
+        System.out.println("Late fee for book: $" + book.calculateLateFee(35)); // 35 days late
+
+        LibraryItem dvd = new LibraryItem("Inception", new DVDLateFee());
+        System.out.println("Late fee for DVD: $" + dvd.calculateLateFee(18)); // 18 days late
+
+        LibraryItem magazine = new LibraryItem("Time Magazine", new MagazineLateFee());
+        System.out.println("Late fee for magazine: $" + magazine.calculateLateFee(10)); // 10 days late
     }
 }
