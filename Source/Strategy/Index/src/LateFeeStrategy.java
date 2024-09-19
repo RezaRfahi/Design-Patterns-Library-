07bbfebc57589c7b8d564544d22519a771dcfd12
RezaRfahi <refahireza82@gmail.com>
2024-09-20 00:58:17 +0330

    LateFeeStrategy Interface codes complated

--- Source/Strategy/Index/src/LateFeeStrategy.java
+++ Source/Strategy/Index/src/LateFeeStrategy.java
@@ -1,2 +1,6 @@
 public interface LateFeeStrategy {
+    double calculateLateFee(int daysLate);
+    double getMaxLateFee(); // New method to return the maximum possible late fee
+    int getGracePeriod();   // New method to return the grace period in days
+    boolean applyAdditionalPenalty(int daysLate);   // New method to check if penalty applies
 }
