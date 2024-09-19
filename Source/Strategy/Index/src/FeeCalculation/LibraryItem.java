422442caa55d1774f5cae192fc3c769970d60d3b
RezaRfahi <refahireza82@gmail.com>
2024-09-20 01:30:44 +0330

    The context class(LibraryItem) was complated

--- Source/Strategy/Index/src/FeeCalculation/LibraryItem.java
+++ Source/Strategy/Index/src/FeeCalculation/LibraryItem.java
@@ -1,4 +1,27 @@
 package FeeCalculation;
 
 public class LibraryItem {
+    private String title;
+    private LateFeeStrategy lateFeeStrategy;
+
+    public LibraryItem(String title, LateFeeStrategy lateFeeStrategy) {
+        this.title = title;
+        this.lateFeeStrategy = lateFeeStrategy;
+    }
+
+    public double calculateLateFee(int daysLate) {
+        double fee = lateFeeStrategy.calculateLateFee(daysLate);
+        if (lateFeeStrategy.applyAdditionalPenalty(daysLate)) {
+            fee += 10.0; // Add $10 penalty if applicable
+        }
+        return fee;
+    }
+
+    public double getMaxLateFee() {
+        return lateFeeStrategy.getMaxLateFee();
+    }
+
+    public int getGracePeriod() {
+        return lateFeeStrategy.getGracePeriod();
+    }
 }
