26b7e9e4846f4622467764bdbcaea2c1a5430f6f
RezaRfahi <refahireza82@gmail.com>
2024-09-20 01:18:57 +0330

    DVDLateFee class was created

---
+++ Source/Strategy/Index/src/FeeCalculation/DVDLateFee.java
@@ -0,0 +1,4 @@
+package FeeCalculation;
+
+public class DVDLateFee {
+}
--- Source/Strategy/Index/src/Main.java
+++ Source/Strategy/Index/src/Main.java
@@ -1,5 +1,8 @@
+import FeeCalculation.BookLateFee;
+
 public class Main {
     public static void main(String[] args) {
-
+        BookLateFee book = new BookLateFee();
+        System.out.println(book.getMaxLateFee());
     }
 }
