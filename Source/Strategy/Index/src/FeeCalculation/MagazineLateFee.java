ebcedfc98e1609d60f32043952a57ab4e0192df5
RezaRfahi <refahireza82@gmail.com>
2024-09-20 01:27:54 +0330

    MagazineLateFee class was complated

--- Source/Strategy/Index/src/FeeCalculation/MagazineLateFee.java
+++ Source/Strategy/Index/src/FeeCalculation/MagazineLateFee.java
@@ -1,4 +1,23 @@
 package FeeCalculation;
 
-public class MagazineLateFee {
+public class MagazineLateFee implements LateFeeStrategy{
+    @Override
+    public double calculateLateFee(int daysLate) {
+        return Math.min(daysLate * 1.50, getMaxLateFee()); // $1.50 per day, max $20
+    }
+
+    @Override
+    public double getMaxLateFee() {
+        return 20.0; // Maximum late fee for DVDs is $20
+    }
+
+    @Override
+    public int getGracePeriod() {
+        return 0; // No grace period for DVDs
+    }
+
+    @Override
+    public boolean applyAdditionalPenalty(int daysLate) {
+        return daysLate > 15; // Additional penalty if more than 15 days late
+    }
 }
