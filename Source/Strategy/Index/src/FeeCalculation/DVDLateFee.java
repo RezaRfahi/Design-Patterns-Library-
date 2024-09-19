930d297af210940ee2b4c453dbbadeb7dbdb2a96
RezaRfahi <refahireza82@gmail.com>
2024-09-20 01:22:43 +0330

    DVDLateFee class was complated

--- Source/Strategy/Index/src/FeeCalculation/DVDLateFee.java
+++ Source/Strategy/Index/src/FeeCalculation/DVDLateFee.java
@@ -1,4 +1,24 @@
 package FeeCalculation;
 
-public class DVDLateFee {
+public class DVDLateFee implements LateFeeStrategy {
+
+    @Override
+    public double calculateLateFee(int daysLate) {
+        return Math.min(daysLate * 1.50, getMaxLateFee()); // $1.50 per day, max $20
+    }
+
+    @Override
+    public double getMaxLateFee() {
+        return 20.0; // Maximum late fee for DVDs is $20
+    }
+
+    @Override
+    public int getGracePeriod() {
+        return 0; // No grace period for DVDs
+    }
+
+    @Override
+    public boolean applyAdditionalPenalty(int daysLate) {
+        return daysLate > 15; // Additional penalty if more than 15 days late
+    }
 }
--- Source/Strategy/Index/src/Main.java
+++ Source/Strategy/Index/src/Main.java
@@ -1,4 +1,4 @@
-import FeeCalculation.BookLateFee;
+import FeeCalculation.*;
 
 public class Main {
     public static void main(String[] args) {
