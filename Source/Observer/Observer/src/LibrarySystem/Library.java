0561afb21a3e2082965727022ab5d844a9194dd0
RezaRfahi <refahireza82@gmail.com>
2024-10-02 02:32:41 +0330

    Library Class Was Completed

--- Source/Observer/Observer/src/LibrarySystem/Library.java
+++ Source/Observer/Observer/src/LibrarySystem/Library.java
@@ -1,4 +1,26 @@
 package LibrarySystem;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Library {
+    private List<Borrowable> items;
+
+    public Library() {
+        items = new ArrayList<>();
+    }
+
+    public void addItem(Borrowable item) {
+        items.add(item);
+        System.out.println(((Book) item).getTitle() + " has been added to the library.");
+    }
+
+    public void showAvailableItems() {
+        System.out.println("Available items in the library:");
+        for (Borrowable item : items) {
+            if (item.isAvailable()) {
+                System.out.println(((Book) item).getTitle());
+            }
+        }
+    }
 }
--- Source/Observer/Observer/src/Main.java
+++ Source/Observer/Observer/src/Main.java
@@ -1,5 +1,6 @@
+import LibrarySystem.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
     }
 }
