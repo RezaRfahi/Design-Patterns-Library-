3592fee8942ab0d9de5673eef396eb1b46a9c0b5
RezaRfahi <refahireza82@gmail.com>
2024-10-02 02:09:26 +0330

    Book Class Was Complated

--- Source/Observer/Observer/src/LibrarySystem/Book.java
+++ Source/Observer/Observer/src/LibrarySystem/Book.java
@@ -1,4 +1,39 @@
 package LibrarySystem;
 
-public class Book {
+public class Book implements Borrowable{
+
+    private String title;
+    private String author;
+    private boolean isAvailable;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+        this.isAvailable = true; // New books are available by default
+         }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public void borrowItem() {
+        if (isAvailable) {
+            isAvailable = false;
+            System.out.println(title + " has been borrowed.");
+        } else {
+            System.out.println(title + " is not available for borrowing.");
+        }
+    }
+
+    @Override
+    public void returnItem() {
+        isAvailable = true;
+        System.out.println(title + " has been returned.");
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return this.isAvailable;
+    }
 }
