a6af391fa7f419eebc294c3ec1899d84cd952e65
RezaRfahi <refahireza82@gmail.com>
2024-10-12 03:05:39 +0330

    The ReservedBook Class has been completed

--- Source/Decorator/src/BookTypes/ReservedBook.java
+++ Source/Decorator/src/BookTypes/ReservedBook.java
@@ -1,7 +1,14 @@
 package BookTypes;
 
 public class ReservedBook extends BookDecorator{
+
     ReservedBook(Book book) {
         super(book);
     }
+
+    @Override
+    public String getDescription() {
+        return decoratedBook.getDescription() + " (Reserved)";
+    }
+
 }
