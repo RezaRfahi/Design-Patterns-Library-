e7cd0b1e4bbe77a121882a9ab88284e18d407dde
RezaRfahi <refahireza82@gmail.com>
2024-10-12 03:11:09 +0330

    The BasicBook Class has been completed as Base Class

--- Source/Decorator/src/BookTypes/BasicBook.java
+++ Source/Decorator/src/BookTypes/BasicBook.java
@@ -1,4 +1,22 @@
 package BookTypes;
 
-public class BasicBook {
+public class BasicBook implements Book{
+
+    private String title;
+    private double price;
+
+    BasicBook(String title, double price){
+        this.title = title;
+        this.price = price;
+    }
+
+    @Override
+    public String getDescription() {
+        return title;
+    }
+
+    @Override
+    public double getCost() {
+        return price;
+    }
 }
