5180e7c65794697eea29df80177fda45ea5dcf08
RezaRfahi <refahireza82@gmail.com>
2024-10-12 02:45:38 +0330

    The BookDecorator Abstract Class has been completed

--- Source/Decorator/src/BookTypes/BookDecorator.java
+++ Source/Decorator/src/BookTypes/BookDecorator.java
@@ -1,6 +1,20 @@
 package BookTypes;
 
 public abstract class BookDecorator implements Book {
-    
 
+    protected Book decoratedBook;
+
+    BookDecorator(Book book) {
+        this.decoratedBook = book;
+    }
+
+    @Override
+    public String getDescription() {
+        return decoratedBook.getDescription();
+    }
+
+    @Override
+    public double getCost() {
+        return decoratedBook.getCost();
+    }
 }
